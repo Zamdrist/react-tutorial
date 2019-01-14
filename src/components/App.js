d365227a8932565a1aa6e0d1d2817260040865a8
Steve <zamdrist@gmail.com>
2019-01-14 05:54:58 -0600

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Table from "./Table";
+
+class App extends Component {
+  removeCharacter = index => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => i !== index)
+    });
+  };
+
+  state = {
+    characters: [
+      {
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        name: "Dee",
+        job: "Aspiring Actress"
+      },
+      {
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+
+  render() {
+    const { characters } = this.state;
+
+    return (
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+      </div>
+    );
+  }
+}
+export default App;
